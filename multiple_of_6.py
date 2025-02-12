baaf3d72c415b7d62f1c5a344bb3fce2feeacef5
bratiu <bratiu@faculty.ie.edu>
2025-02-12 17:59:34 +0100

    added multiple of 6

---
+++ multiple_of_6.py
@@ -0,0 +1,20 @@
+def get_multiple_6():
+    """
+    Returns a multiple of 6 that was entered by the user.
+    :return: int a number
+    """
+    while True:
+        try:
+            n = input("Please give me a multiple of 6: ")
+            n = int(n) # convert to int
+            # how to check if it is a multiple of 6?
+            # another way to check:
+            if n / 6 == n // 6:
+                return n
+            else:
+                print("that is not a multiple of 6")
+        except ValueError:
+            print("you have not entered a number")
+
+
+print(get_multiple_6())
